0ad6108ba80ed4a245cdac85bedb7d68d3f6d34e
Taylor Laine <tlaine@msudenver.edu>
2017-03-28 15:25:59 -0600

    adds style and more logic

--- js/app.js
+++ js/app.js
@@ -115,6 +115,13 @@ const showPlayBtn = function() {
     document.getElementById('ready-to-play').className="ready-to-play show-div";  
 }
 
+const hidePlayBtn = function() {
+    document.getElementById('ready-to-play').className="ready-to-play hidden-div";  
+}
+
+const showGame = function() {
+	 document.getElementById('main-game').className="show-div";  
+}
 
 let player1 = createPlayer();
 let monster1 = createMonster();
@@ -126,52 +133,76 @@ nextBtn.addEventListener("click", function() {
 	player1.setName(characterName);
 	hideNameInput();
 	showPlayBtn();
+	generateStartBtn();
 	console.log(player1.getName());
 
 });
 
-// select attack button on dom
-let attackBtn = document.getElementById("attack");
-// add event listener to attackBtn
-attackBtn.addEventListener("click", function() {
-	// if monster 1's health is greater than 0
-	if(monster1.getHealth() > 0) {
-		// player attack monster
-		player1.attack(monster1);
-		// disable attackBtn while monster attacks
-		attackBtn.disabled = true;
-		// create a p node to append player damge to dom 
-		let playerDamageNode = document.createElement("p");
-			// create damge string to put inside damage node
-			let playerDamageInfo = (player1.getName() + " attacks: " + monster1.getName() +  " health: " + monster1.getHealth());
-			// pass damage info string into playerDamageNode
-			playerDamageNode.innerHTML = playerDamageInfo;
-			// append playerDamageNode to div with id #damage
-			document.getElementById("damage").appendChild(playerDamageNode);
-		console.log(player1.getName() + " attacks: " + monster1.getName() +  " health: " + monster1.getHealth());
-			// set 2 sec timeout for monster attack delay
-			setTimeout(function(){
-				// if monster health is > 0 allow attack
-				if (monster1.getHealth() > 0) {
-					// monster attack player
-					monster1.attack(player1);
-					// create p node to append monster damage to dom
-					let monsterDamageNode = document.createElement("p");
-					// // create damge string to put inside damage node
-					let monsterDamageInfo = (monster1.getName() + " attacks: " + player1.getName() +  " health: " + player1.getHealth());
-					// append monsterDamageNode to div with id #damage
-					monsterDamageNode.innerHTML = monsterDamageInfo;
-					document.getElementById("damage").appendChild(monsterDamageNode);
-					console.log(monster1.getName() + " attacks: " + player1.getName() +  " health: " + player1.getHealth());
-				}
-				// set 2sec delay to reactivate player attack button
+const startGame = function() {
+	showGame();
+	// select attack button on dom
+	let attackBtn = document.getElementById("attack");
+	// add event listener to attackBtn
+	attackBtn.addEventListener("click", function() {
+		// if monster 1's health is greater than 0
+		if(monster1.getHealth() > 0) {
+			// player attack monster
+			player1.attack(monster1);
+			// disable attackBtn while monster attacks
+			attackBtn.disabled = true;
+			// create a p node to append player damge to dom 
+			let playerDamageNode = document.createElement("p");
+				// create damge string to put inside damage node
+				let playerDamageInfo = (player1.getName() + " attacks: " + monster1.getName() +  " health: " + monster1.getHealth());
+				// pass damage info string into playerDamageNode
+				playerDamageNode.innerHTML = playerDamageInfo;
+				// append playerDamageNode to div with id #damage
+				document.getElementById("damage").appendChild(playerDamageNode);
+			console.log(player1.getName() + " attacks: " + monster1.getName() +  " health: " + monster1.getHealth());
+				// set 2 sec timeout for monster attack delay
 				setTimeout(function(){
-					// reactivate attackBtn
-					attackBtn.disabled = false;
-				}, 2000);
-		}, 2000);
-	} else {
-		console.log(monster1.getName() + " is dead");
-	}
+					// if monster health is > 0 allow attack
+					if (monster1.getHealth() > 0) {
+						// monster attack player
+						monster1.attack(player1);
+						// create p node to append monster damage to dom
+						let monsterDamageNode = document.createElement("p");
+						// // create damge string to put inside damage node
+						let monsterDamageInfo = (monster1.getName() + " attacks: " + player1.getName() +  " health: " + player1.getHealth());
+						// append monsterDamageNode to div with id #damage
+						monsterDamageNode.innerHTML = monsterDamageInfo;
+						document.getElementById("damage").appendChild(monsterDamageNode);
+						console.log(monster1.getName() + " attacks: " + player1.getName() +  " health: " + player1.getHealth());
+					}
+					// set 2sec delay to reactivate player attack button
+					setTimeout(function(){
+						// reactivate attackBtn
+						attackBtn.disabled = false;
+					}, 2000);
+			}, 2000);
+		} else {
+			console.log(monster1.getName() + " is dead");
+		}
 
-});
+	});
+}
+
+const generateStartBtn = function() {
+	let div = document.getElementById("ready-to-play-content");
+	let h1 = document.createElement('h1');
+	h1.innerHTML = (player1.name + " are you ready?");
+	div.appendChild(h1);
+	let button = document.createElement("button");
+	button.innerHTML = "Play";
+	button.id = "play-btn"
+	div.appendChild(button);
+
+	let playBtn = document.getElementById("play-btn");
+	playBtn.addEventListener("click", function() {
+	hidePlayBtn();
+	startGame();
+	});
+}
+
+
+
